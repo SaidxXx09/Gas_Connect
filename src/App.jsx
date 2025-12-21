29051d421755ada568cb3b73160df7a9f0af6de8
alejoturip <alejandrofabara1@gmail.com>
2025-12-21 18:11:36 -0500

    INTEGRACION DE AOS PARA EFECTO AL SCROLLEAR

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { useEffect } from "react"; // 1. Importamos useEffect
+import AOS from "aos";             // 2. Importamos la librería
+import "aos/dist/aos.css";        // 3. Importamos los estilos de AOS
+
 import Landing from "./pages/Landing";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -6,6 +10,14 @@ import Dashboard from "./pages/Dashboard";
 import NotFound from "./pages/NotFound";
 
 function App() {
+  // 4. Inicializamos AOS cuando cargue la App
+  useEffect(() => {
+    AOS.init({
+      duration: 1000, // La animación durará 1 segundo
+      once: false,    // Esto hace que la animación se repita al subir y bajar
+      mirror: true,   // Animaciones al hacer scroll hacia arriba también
+    });
+  }, []);
 
   return (
     <BrowserRouter>
@@ -17,8 +29,7 @@ function App() {
         <Route path='*' element={<NotFound />} />
       </Routes>
     </BrowserRouter>
-
-  )
+  );
 }
 
 export default App;
